63cdf693616ed3b7c153f8de415d04fdaad9b62d
Elise Valkanas <evalkanas@gmail.com>
2021-05-06 11:03:48 -0400

    start mutect to MF conversion

---
+++ mosaicforecast/Mutect_to_MF.sh
@@ -0,0 +1,1 @@
+### Code to convert Mutect vcf output to input for MosaicForecast. Adapted from MosaicForecast Snakemake file (https://github.com/parklab/MosaicForecast/blob/master/Snakemake/Snakefile)
